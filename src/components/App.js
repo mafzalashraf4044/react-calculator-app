294e67306f49e8ecf78ba25693af389a4709817a
Afzal Ashraf <afzalashraf.dev@gmail.com>
2017-09-01 23:08:30 +0500

    constant created for calculator keys

--- src/components/App.js
+++ src/components/App.js
@@ -2,23 +2,57 @@ import React, {
     Component
 } from 'react';
 
+import * as keys from '../constants/keypadConstants'; 
+
 //Third Party Components
 import { Grid, Row, Col } from 'react-bootstrap';
+
+//Custom Components
+import Keypad from './Keypad';
+import DisplayScreen from './DisplayScreen';
 
 export default class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            keys: [
+                keys.BACK_SPACE,
+                keys.PERCENTAGE,
+                keys.DIVIDE,
+                keys.MULTIPLY,
+                keys.SEVEN,
+                keys.EIGHT,
+                keys.NINE,
+                keys.MINUS,
+                keys.FOUR,
+                keys.FIVE,
+                keys.SIX,
+                keys.PLUS,
+                keys.ONE,
+                keys.TWO,
+                keys.THREE,
+                keys.SQUARE_ROOT,
+                keys.ZERO,
+                keys.DECIMAL,
+                keys.SIGN,
+                keys.EVALUATE
+            ]
+
         };
+    }
+
+    onKeyPress = (event) => {
+        console.log("event", event.target)
     }
 
     render() {
         return (
             <Grid className="main-container" fluid={true}>
-                <Row className="main-row">
+                <Row>
                     <Col xs={10} xsOffset={1} sm={8} smOffset={2} md={4} mdOffset={4}>
                         <div className="calculator">
-
+                            <DisplayScreen />
+                            <Keypad keys={this.state.keys} onKeyPress={this.onKeyPress}/>
                         </div>
                     </Col>
                 </Row>
---
+++ src/constants/keypadConstants.js
@@ -0,0 +1,25 @@
+//Numbers
+export const ZERO  = "0";
+export const ONE   = "1";
+export const TWO   = "2";
+export const THREE = "3";
+export const FOUR  = "4";
+export const FIVE  = "5";
+export const SIX   = "6";
+export const SEVEN = "7";
+export const EIGHT = "8";
+export const NINE  = "9";
+
+//Operators
+export const PLUS        = "+";
+export const MINUS       = "-";
+export const MULTIPLY    = "x";
+export const DIVIDE      = "/";
+export const DECIMAL     = ".";
+export const PERCENTAGE  = "%";
+export const SQUARE_ROOT = "sq-root";
+export const SIGN        = "+-"
+
+//Operations
+export const EVALUATE   = "=";
+export const BACK_SPACE = "bk-space";
